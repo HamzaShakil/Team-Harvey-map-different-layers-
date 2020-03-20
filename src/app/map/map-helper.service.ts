df7cff5d5bf8099a87dc8bd8dc08c2c03db483e9
HamzaShakil <2hamzashakil96@gmail.com>
2020-03-20 10:30:19 +0500

    6 files modified, 1 added

--- src/app/map/map-helper.service.ts
+++ src/app/map/map-helper.service.ts
@@ -1,8 +1,48 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
+import L from 'leaflet'
 @Injectable()
 export class MapHelperService {
 
-  constructor() { }
+  public map;
+
+   initMap(){
+     this.map = new L.Map("map");
+     this.map.setView(new L.LatLng(31.585692323629303, 35.19333585601518), 5);
+     L.tileLayer(`https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png`, {
+             maxZoom: 20,
+             attribution: 'HOT',
+             
+
+          }).addTo(this.map);
+          
+   } 
+   drawPoints(data){
+      // points
+      // data.points.forEach(p=>this.addMarker(p.pos.lat, p.pos.lon));
+      // lines
+      data.lines.forEach(l=>{
+        // src
+        this.addMarker(l.pos.src.lat, l.pos.src.lon);
+        // dest
+        this.addMarker(l.pos.dest.lat, l.pos.dest.lon);
+        // line       
+        this.addLine(l.pos.src, l.pos.dest);             
+      });
+   }
+   addMarker(lat,lng){
+    let m = L.marker([lat,lng]).addTo(this.map);
+   }
+   addLine(src, dest) {
+         let line = L.polyline(
+                [
+                    [src.lat, src.lon],
+                    [dest.lat, dest.lon]
+                ],
+                {color: 'red'}
+         ).addTo(this.map)
+   }
 
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnChanges, AfterViewInit } from '@angular/core';
+import {GeocodesService}  from '../geocodes.service'
+import {MapHelperService} from './map-helper.service'
 @Component({
-  selector: 'app-map',
+  selector: 'map-cmp',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: [ './map.component.css' ],
+  providers:[MapHelperService]
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnChanges, AfterViewInit {
+  map;
+  constructor(private geo:GeocodesService, private helper:MapHelperService){
 
-  constructor() { }
+  }
 
-  ngOnInit() {
+  ngOnChanges(ch) {
+    
+  }
+
+  ngAfterViewInit() {
+    this.helper.initMap();
+    this.geo.getPositions().subscribe(geo=>{
+      console.log(`lll`)
+      this.helper.drawPoints(geo)
+    })
   }
 
 }
